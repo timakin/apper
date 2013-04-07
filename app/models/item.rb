2618dda0576e1ecf765d11eebdacaf05e2f65f74
timakin <timaki.st@gmail.com>
2013-04-07 22:53:42 +0900

    carrerwave

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
-  attr_accessible :text, :title
+  attr_accessible :text, :title, :image
   belongs_to :user
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407115110) do
+ActiveRecord::Schema.define(:version => 20130407133547) do
 
   create_table "items", :force => true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
   create_table "users", :force => true do |t|
