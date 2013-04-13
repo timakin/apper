ba907fa2b624466ca53aea1f5ca4db340d6fa699
timakin <timaki.st@gmail.com>
2013-04-13 22:35:42 +0900

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Apper::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'items#index'
 
   # See how all your routes lay out with "rake routes"
 
